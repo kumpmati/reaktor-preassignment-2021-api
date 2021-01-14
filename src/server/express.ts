bc8e3a234d262366cecab185b83515f2377254e8
mvkump <mvkump@utu.fi>
2021-01-14 14:04:38 +0200

    disabled client-side caching

--- src/server/express.ts
+++ src/server/express.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import nocache from "nocache";
 import { apiRoutes } from "../api";
 import { mockApiRoutes } from "../mock/api";
 import { Config } from "../types";
@@ -8,6 +9,7 @@ export const startExpress = (config: Config) => {
   const app = express();
 
   app.use(cors());
+  app.use(nocache());
   app.use("/api", config.mock ? mockApiRoutes : apiRoutes);
 
   return app.listen(config.port, () =>
